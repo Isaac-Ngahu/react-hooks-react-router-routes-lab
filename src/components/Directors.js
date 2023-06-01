8dd82016dd4fe7d0956fb7742d05fa47f778d73e
Isaac-ngahu <isaac.ngahu@student.moringaschool.com>
2023-06-01 16:16:27 +0300

    done

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,27 @@
 import React from "react";
 import { directors } from "../data";
-
+// console.log(directors.movies)
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  
+  return <div>
+<h1>Directors Page</h1>
+{directors.map((director)=>{
+  return <div key={director.name}>
+<h3>Name:{director.name}</h3>
+<ul>Movies:{director.movies.map((movie)=>{
+ return <li key={movie}>{movie}</li>
+})}</ul>
+</div>
+
+})}
+{/* {directors.map((name)=>{
+  return <h3 key={name}>Name: {name}</h3>
+})}
+<ul>Movies: {directors.movies.map((movie)=>{
+  return <li key={movie}>{movie}</li>
+})}</ul>
+  </div>) */}
+</div>
 }
 
 export default Directors;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,21 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-
+const linkStyles = {
+  display: "inline-block",
+  width: "50px",
+  padding: "13px",
+  margin: "0 6px",
+  background: "blue",
+  textDecoration: "none",
+  color: "white",
+};
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return <nav>
+    <NavLink exact to="/" style={linkStyles} activeStyle={{background:"maroon"}}>Home</NavLink>
+    <NavLink exact to="/movies"style={linkStyles}  activeStyle={{background:"maroon"}}>Movies</NavLink>
+    <NavLink exact to="/directors" style={linkStyles}  activeStyle={{background:"maroon"}}>Directors</NavLink>
+    <NavLink exact to="/actors" style={linkStyles}  activeStyle={{background:"maroon"}}>Actors</NavLink>
+  </nav>;
 }
 
 export default NavBar;
